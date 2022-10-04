c075e0dad52fea809b661f94f59ebd2b715e9934
Nicolas Romano <nicolasromano2@gmail.com>
2022-10-04 20:51:47 -0300

    creacion proyecto

---
+++ salud/src/test/java/com/egg/salud/SaludApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egg.salud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaludApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
