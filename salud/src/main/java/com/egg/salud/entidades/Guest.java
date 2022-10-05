77a78a90520fbc97d78b870fb7927fc5dbba914b
Emiliano Donoso <emilianodonoso@outlook.com>
2022-10-05 11:38:47 -0300

    creacion base de datos

--- salud/src/main/java/com/egg/salud/entidades/Guest.java
+++ salud/src/main/java/com/egg/salud/entidades/Guest.java
@@ -2,8 +2,11 @@
 package com.egg.salud.entidades;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -28,6 +31,9 @@ public class Guest extends Usuario{
    
    private String nacionalidad;
    private String urlFoto;
+   
+    @OneToMany(mappedBy ="idGuest", cascade=CascadeType.ALL)
+    private List<Oferta> oferta;
 
     public Guest(String email, String nombre, String apellido, long dni, String obra_social, long telefono, Date fecha_nac, String nacionalidad, String urlFoto, Long id, String usuario, String contraseña, boolean estado, Set<Rol> roles) {
         super(id, usuario, contraseña, estado, roles);
