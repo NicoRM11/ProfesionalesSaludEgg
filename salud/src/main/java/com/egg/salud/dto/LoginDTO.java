29e7c5822cf69c8ba25a40ab7b75be6e9bf83d91
Santiago Niveyro <santiok95@gmail.com>
2022-10-06 20:49:17 -0300

    agregando algunas cosas

---
+++ salud/src/main/java/com/egg/salud/dto/LoginDTO.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.egg.salud.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+    
+    
+    
+    
+   @
+   private String email;
+   private String nombre;
+   private String apellido;
+   private long dni;
+   private String obra_social;
+   private long telefono;
+    
+    
+    
+}
---
+++ salud/src/main/java/com/egg/salud/dto/RegistroGuestDTO.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.egg.salud.dto;
+
+import java.util.Date;
+import lombok.Data;
+
+/**
+ *
+ * @author santi
+ */
+@Data
+public class RegistroGuestDTO {
+    
+    
+   
+   private String email;
+   private String nombre;
+   private String apellido;
+   private long dni;
+   private String obra_social;
+   private long telefono;
+   
+   private Date fecha_nacimiento;
+   
+   private String nacionalidad;
+   
+   private String url_foto;
+   
+   
+   
+   
+}
