8cf577e57aa3c17540f11e54128d6585b473ca20
Santiago Niveyro <santiok95@gmail.com>
2022-10-16 15:37:27 -0300

    commit de seguridad

--- salud/src/main/java/com/egg/salud/configuracion/SecurityConfig.java
+++ salud/src/main/java/com/egg/salud/configuracion/SecurityConfig.java
@@ -4,19 +4,26 @@
  */
 package com.egg.salud.configuracion;
 
+import com.egg.salud.seguridad.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
+    
+    @Autowired
+	private CustomUserDetailsService userDetailsService;
+    
     
     @Bean
 	PasswordEncoder passwordEncoder() {
@@ -24,12 +31,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 
 	}
         
-    @Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/**").permitAll()
-				.and().csrf().disable();
-
-
+    @Bean
+	protected  SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+                        .antMatchers(HttpMethod.GET, "/api/**").permitAll()
+                        .antMatchers("/api/auth/**").permitAll()
+                        .anyRequest()
+                        .authenticated();
+                http.httpBasic();
+                http.formLogin();
+                
+                        
+                return http.build();
 
 	}
-}
+        
+	protected void defaultSecurityFilterChain(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+	
+//	@Override
+//	@Bean
+//	public AuthenticationManager authenticationManagerBean() throws Exception {
+//		return super.authenticationManagerBean();
+	}
+        
+        
