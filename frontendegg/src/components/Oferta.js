d3c5a3a33f2785a28225c924678edbd9549dcf39
Hernan Figueroa <gabriel_hernan_2107@hotmail.com>
2022-10-23 16:11:04 -0300

    update oferta - createOption

--- frontendegg/src/components/Oferta.js
+++ frontendegg/src/components/Oferta.js
@@ -4,88 +4,130 @@ import moment from 'moment';
 import DatePicker from "react-datepicker";
 import Swal from 'sweetalert2';
 import { CustomEvent } from './CustomEvent';
+import axios from 'axios';
 require('moment/locale/es.js');
 
 const events = [
     {
         id: "1",
         modalidad: "Virtual",
-        paciente: "Serena Williams",
+        paciente: "Serena Williams", // recibir objeto guest
         especialidad: "Ginecologia",
+        consultorio: "",
         localidad: "Escobar",
+        telefono: "15122331",
         start: new Date(2022, 10, 1),
         end: new Date(2022, 10, 1),
-        isBooked: true
+        disponible: true
     },
     {
         id: "2",
         modalidad: "Virtual",
         paciente: "",
         especialidad: "Cardiologia",
+        consultorio: "",
         localidad: "Escobar",
         telefono: "15122331",
         start: new Date(2022, 9, 7),
         end: new Date(2022, 9, 10),
-        isBooked: false
+        disponible: false
     },
     {
         id: "3",
         modalidad: "Presencial",
         paciente: "",
         especialidad: "Clinica",
+        consultorio: "",
         localidad: "Escobar",
         telefono: "15884333",
         start: new Date(2022, 9, 20),
         end: new Date(2022, 9, 23),
-        isBooked: false
+        disponible: false
     },
 ];
 
 export const Oferta = () => {
     const localizer = momentLocalizer(moment);
-    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+    const [newEvent, setNewEvent] = useState({ start:"", end:"",consultorio:"", modalidad:"", telefono:"", localidad:"" });
     const [allEvents, setAllEvents] = useState(events);
-    const [selected, setSelected] = useState();
+    const [data,setData] = useState({start:"", end:"",consultorio:"", modalidad:"", telefono:"", localidad:""});
+    const [selected, setSelected] = useState(); 
 
-    const handleAddEvent = () => {
-        setAllEvents([...allEvents, newEvent]);
-    }
+    const username = JSON.parse(localStorage.getItem('usuario'))
+    const password = JSON.parse(localStorage.getItem('password'))
 
     const handleSelected = (event) => {
         setSelected(event);
         Swal.fire(`Modalidad: ${event.modalidad},\n
         Telefono: ${event.telefono}, \n
-        Estado: ${event.isBooked ? "oferta reservada": "oferta disponible"},\n
+        Estado: ${event.disponible ? "oferta reservada": "oferta disponible"},\n
         Paciente: ${event.paciente ? event.paciente : " --"}`)
     };
+
+    const URL = `http://localhost:8080/api/oferta/crear-oferta/${username}`;
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        setAllEvents([...allEvents, newEvent]);
+        setData({start:newEvent.start, end:newEvent.end, modalidad:newEvent.modalidad, telefono:newEvent.telefono, localidad:newEvent.localidad})
+        try {
+            const response = await axios.post(URL, data, {
+                auth: {
+                    username: `${username}`,
+                    password: `${password}`
+                }
+            })
+
+            console.log(response);
+            if (response.status === 200) {
+                Swal.fire(
+                    'Excelente!',
+                    'La oferta ha sido creada exitosamente',
+                    'success'
+                )
+            }
+
+        } catch (error) {
+            if (error.response.status === 406) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: `No se pudo crear, ${error.response.data} !`,
+                })
+            }
+            console.log(error)
+        }
+    }
 
     return (
         <div className="myCustomHeight">
             <h1 className="text-center">Calendario</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="row justify-content-center" style={{ position: "relative", zIndex: "999" }}>
-                    <div className="col-auto">
+                    <div className="col">
                         <input className="form-control rounded-2" required type="text" placeholder="Telefono" value={newEvent.telefono} onChange={(e) => setNewEvent({ ...newEvent, telefono: e.target.value })} />
                     </div>
-                    <div className="col-auto">
+                    <div className="col">
                         <input className="form-control rounded-2"  type="text" placeholder="Localidad" value={newEvent.localidad} onChange={(e) => setNewEvent({ ...newEvent, localidad: e.target.value })} />
                     </div>
-                    <div className="col-auto">
+                    <div className="col">
+                        <input className="form-control rounded-2"  type="text" placeholder="Consultorio" value={newEvent.consultorio} onChange={(e) => setNewEvent({ ...newEvent, consultorio: e.target.value })} />
+                    </div>
+                    <div className="col">
                         <select className="form-select" value={newEvent.modalidad} name="modalidad" onChange={(e) => setNewEvent({ ...newEvent, modalidad: e.target.value })}>
                             <option value="Virtual">Virtual</option>
                             <option value="Presencial">Presencial</option>
                         </select>
                     </div>
-                    <div className="col-auto">
+                    <div className="col">
                         <DatePicker className="form-control rounded-2" required placeholderText="Start Date" dateFormat="Pp" showTimeSelect selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
 
                     </div>
-                    <div className="col-auto">
+                    <div className="col">
                         <DatePicker className="form-control rounded-2" required tabindex="10" placeholderText="End Date" dateFormat="Pp" showTimeSelect selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
 
                     </div>
                     <div className="col-auto">
-                        <button className="btn btn-success" onClick={handleAddEvent}>
+                        <button className="btn btn-success">
                             Crear Oferta
                         </button>
                     </div>
@@ -111,7 +153,7 @@ export const Oferta = () => {
                     day: "Día"
                 }}
                 eventPropGetter={(event) => {
-                    const backgroundColor = event.isBooked ? '#66bf4f' : '#ec6434';
+                    const backgroundColor = event.disponible ? '#66bf4f' : '#ec6434';
                     return { style: { backgroundColor } }
                 }}
             />
