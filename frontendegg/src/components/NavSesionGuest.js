d32bc2588810673907bbd7e283adb6a6e2bb9288
Cecilia Longhi <mcecilia.longhi1@gmail.com>
2022-10-28 19:19:14 -0300

    update navbar

--- frontendegg/src/components/NavSesionGuest.js
+++ frontendegg/src/components/NavSesionGuest.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
-import Row from 'react-bootstrap/Row';
+
 import axios from 'axios';
 import Avatar from "@mui/material/Avatar";
 import { GuestProfile } from './GuestProfile';
@@ -17,9 +17,7 @@ export const NavSesionGuest = () => {
     const username = JSON.parse(localStorage.getItem('usuario'))
     const URL = `http://localhost:8080/api/guest/detalle/${username}`;
 
-    useEffect(() => {
-        cargarPerfil();
-    }, []);
+
     const cargarPerfil = async () => {
         try {
             const response = await axios.get(URL, {
@@ -44,21 +42,21 @@ export const NavSesionGuest = () => {
 
 
     return (
-        <div className="nav-item active">
+        <div className="nav-item active d-flex flex-row align-items-center">
 
-            <Row>
+            
 
             <Link to={'/GuestProfile'} className="text-decoration-none ">
                 
-                <Avatar
+                <Avatar className=""
                     alt="Imagen Perfil"
                     src={data.urlFoto}
-                    sx={{ width: 50, height: 50 }}
+                    sx={{ width: 40, height: 40 }}
                 />
                     
                 </Link>
-                <span className="text-white"> Hola, {data.nombre}</span>
-            </Row>
+                <h6 className="text-white m-2 "> Hola, {data.nombre}</h6>
+            
 
 
         </div>
